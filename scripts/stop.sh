5a90a3e0cf9a03739f7b430a9bbdb660181925b6
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-12-05 23:06:34 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/catchyou.jar"
+# Api 중지
+echo "Stopping Api module..."
+pkill -f "java -jar /home/ubuntu/app/Api/build/libs/Api.jar"
 
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+# Domain 중지
+echo "Stopping Domain module..."
+pkill -f "java -jar /home/ubuntu/app/Domain/build/libs/Domain.jar"
 
-TIME_NOW=$(date +%c)
+# Infrastructure 중지
+echo "Stopping Infrastructure module..."
+pkill -f "java -jar /home/ubuntu/app/Infrastructure/build/libs/Infrastructure.jar"
 
-#현재 구동 중인 애플리케이션 pid 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-
-#프로세스가 켜져 있으면 종료
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행 중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행 중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 $CURRENT_PID
-fi
+# Core 중지
+echo "Stopping Core module..."
+pkill -f "java -jar /home/ubuntu/app/Core/build/libs/Core.jar"
