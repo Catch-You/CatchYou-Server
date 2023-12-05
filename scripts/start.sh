334423dd1e060a578a020a1e016e304bcb5717f3
SeungYeon Ahn <88308933+xloyeon@users.noreply.github.com>
2023-12-05 23:44:43 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+PROJECT_ROOT="/home/ubuntu/app"
+
 # Core 실행
 echo "Starting Core module..."
-bash ./start-core.sh
+bash $PROJECT_ROOT/scripts/start-core.sh
 
 # Infrastructure 실행
 echo "Starting Infrastructure module..."
-bash ./start-infrastructure.sh
+bash $PROJECT_ROOT/scripts/start-infrastructure.sh
 
 # Domain 실행
 echo "Starting Domain module..."
-bash ./start-domain.sh
+bash $PROJECT_ROOT/scripts/start-domain.sh
 
 # Api 실행
 echo "Starting Api module..."
-bash ./start-api.sh
+bash $PROJECT_ROOT/scripts/start-api.sh
 
