9f5a080a4fe3391b7f6bd171a96cf82110ad9dbf
syahn <496300@naver.com>
2023-12-09 11:37:40 +0900

    chore: OpenFeign 사용 설정

--- Api/src/main/java/com/catchyou/CatchyouApiApplication.java
+++ Api/src/main/java/com/catchyou/CatchyouApiApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Arrays;
 
+@EnableFeignClients
 @RequiredArgsConstructor
 @SpringBootApplication(scanBasePackages = "com.catchyou")
 @Slf4j
