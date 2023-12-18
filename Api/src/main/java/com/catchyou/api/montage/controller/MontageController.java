78af5d268e28812fda19593f7132f857fbbb22ec
syahn <496300@naver.com>
2023-12-19 00:15:24 +0900

    refactor: 몽타주 생성 요청 받을 시 request body 사용

--- Api/src/main/java/com/catchyou/api/montage/controller/MontageController.java
+++ Api/src/main/java/com/catchyou/api/montage/controller/MontageController.java
@@ -16,7 +16,7 @@ public class MontageController {
 
     @PostMapping("/create/{interviewId}")
     public MontageResponse createMontage(@PathVariable Long interviewId,
-                                         @RequestParam String prompt){
-        return montageService.createMontage(interviewId, prompt);
+                                         @RequestBody @Valid CreateMontageRequest request){
+        return montageService.createMontage(interviewId, request);
     }
 }
